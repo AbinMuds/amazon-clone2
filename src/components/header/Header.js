b668da4ac56da2f65d72b4bb1d4fbd948e921574
AbinMuds <abinashmudbhari@gmail.com>
2022-04-14 01:47:16 -0600

    initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header_logo"
+        src="https://png2png.com/wp-content/uploads/2021/08/images-9.png"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Header;

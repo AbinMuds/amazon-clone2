d50ba95bbe20f498c899dfd8150fd03ffd466cdf
AbinMuds <abinashmudbhari@gmail.com>
2022-04-14 15:56:05 -0600

    adding additional components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Route, Switch } from "react-router-dom";
+import Layout from "./components/header/Layout";
 import HomePage from "./components/home/HomePage";
 
 function App() {
   return (
-    <Switch>
-      <Route to="/">
-        <HomePage />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route to="/">
+          <HomePage />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
